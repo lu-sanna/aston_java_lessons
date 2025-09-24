4cb823d3ed784e9db06cfa3111a1d755e2a7ca01
lu-sanna <yuliss57@yandex.ru>
2025-09-24 17:28:57 +0300

    ветка для домашнего задания 2.7_testNG

--- src/test/java/lesson2_7_TestNG/TriangleAreaTestNg.java
+++ src/test/java/lesson2_7_TestNG/TriangleAreaTestNg.java
@@ -2,6 +2,7 @@ package lesson2_7_TestNG;
 
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
+import org.testng.annotations.DataProvider;
 
 public class TriangleAreaTestNg {
     @Test
@@ -12,5 +13,19 @@ public class TriangleAreaTestNg {
     @Test(expectedExceptions = IllegalArgumentException.class)
     public void testAreaInvalidVal() {
         TriangleArea.area(-10, 5);
+    }
+
+    @DataProvider (name = "areaData")
+    public Object[][] areaData() {
+        return new Object[][]{
+                {10.0, 5.0, 25.0},
+                {5.0, 5.0, 12.5},
+                {8.0, 3.0, 12.0}
+        };
+    }
+
+    @Test(dataProvider = "areaData")
+    public void testCalculateAreaParameterized(double a, double h, double expected) {
+        assertEquals(TriangleArea.area(a, h), expected, 0.001);
     }
 }
